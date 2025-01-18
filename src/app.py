970a7b6edff99f5fae026345d6787b270d8680ac
19Juan <19juan.cortes@gmail.com>
2025-01-18 00:37:06 +0000

    venv

--- src/app.py
+++ src/app.py
@@ -5,3 +5,8 @@ from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+spotipy.spotify(auth=client_id, client_secret=client_secret)
+
