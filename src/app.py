bb4909eacecb4e8bf3d3a2d944b0976545bf3ee9
19Juan <19juan.cortes@gmail.com>
2025-01-18 00:48:01 +0000

    adelanto

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 # load the .env file variables
 load_dotenv()
 
@@ -10,9 +11,7 @@ client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 spotipy.spotify()
 
-from spotipy.oauth2 import SpotifyClientCredentials
-
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+birdy_uri = '7FBcuc1gsnv6Y1nwFtNRCb?trackId=4pvknJEcQuzyCj1b7KIkac'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 results = spotify.artist_albums(birdy_uri, album_type='album')
